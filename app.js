6344d6aaeeff09cdf2db4b9a271669c005b5d152
brudolce <brudolce@gmail.com>
2019-02-14 11:30:46 -0200

    MONGODB_URI

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/User');
 
 mongoose
-  .connect('mongodb://localhost/simpleagenda', { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then((x) => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
